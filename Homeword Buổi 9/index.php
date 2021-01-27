73601aa2bbfa26763eb65f3c636501f3d5903b4a
username <your_email>
2021-01-27 17:27:20 +0700

    Homeword uint 9 mvc 1

---
+++ Homeword Buổi 9/index.php
@@ -0,0 +1,19 @@
+ <?php 
+
+$mod = $_GET['mod'];
+$act = $_GET['act'];
+$params = isset($_GET['id']) ? [$_GET['id']] : [];
+// include_once('controller/CategoryController.php');
+// include_once('controller/PostController.php');
+
+$class = ucfirst($mod)."Controller";
+$path = "controller/$class.php";
+
+	require_once($path);
+
+	$controller_obj = new $class();
+	$data = $_POST;
+	$params[] = $data;
+	call_user_func_array([$controller_obj,$act],$params);
+
+ ?>
